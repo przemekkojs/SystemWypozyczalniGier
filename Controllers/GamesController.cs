a587e38dfa8db37746916a7802320e587751e4fd
lukaszbielawski <44624897+lukaszbielawski@users.noreply.github.com>
2023-12-29 20:42:21 +0100

    gamecontroller create view working

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -49,25 +49,115 @@ namespace SystemWypozyczalniGier.Controllers
         public IActionResult Create()
         {
             ViewData["PublisherId"] = new SelectList(_context.Publishers, "PublisherId", "Name");
-            return View();
+            var model = new Game
+            {
+                MainPhotoName = "defaultMainPhotoName",
+                Photo1Name = "defaultPhoto1Name"
+            };
+
+            return View(model);
         }
 
-        // POST: Games/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("GameId,PublisherId,Title,Description,Price,QuantityInStock,MaxQuantity,Accessibility,Discount,Pegi")] Game game)
+        public async Task<IActionResult> Create(Game model, IFormFile mainPhoto, List<IFormFile> thumbnailPhotos)
         {
+            if (mainPhoto != null && mainPhoto.Length > 0)
+            {   
+                string timestamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                string fileName = $"{timestamp}_0{Path.GetExtension(mainPhoto.FileName).ToLowerInvariant()}";
+
+                var rootPath = Path.GetFullPath("wwwroot");
+                var uploadPath = Path.Combine(rootPath, "uploads");
+                var filePath = Path.Combine(uploadPath, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    mainPhoto.CopyTo(fileStream);
+                    model.MainPhotoName = fileName;
+                }
+            }
+            else
+            {
+                return RedirectToAction(nameof(Create));
+            }
+
+            if (thumbnailPhotos != null && thumbnailPhotos.Count > 0)
+            {
+                int photoIndex = 1;
+                foreach (var thumbnailPhoto in thumbnailPhotos)
+                {   
+                    string timestamp = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                    string fileName = $"{timestamp}_{photoIndex}{Path.GetExtension(thumbnailPhoto.FileName).ToLowerInvariant()}";
+                    var rootPath = Path.GetFullPath("wwwroot");
+                    var uploadPath = Path.Combine(rootPath, "uploads");
+                    var filePath = Path.Combine(uploadPath, fileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        thumbnailPhoto.CopyTo(fileStream);
+                        switch (photoIndex)
+                        {
+                            case 1:
+                                model.Photo1Name = fileName;
+                                break;
+                            case 2:
+                                model.Photo2Name = fileName;
+                                break;
+                            case 3:
+                                model.Photo3Name = fileName;
+                                break;
+                            default:
+                                model.Photo4Name = fileName;
+                                break;
+                        }
+
+                    }
+                    photoIndex++;
+                }
+            }
+            else
+            {
+                return RedirectToAction(nameof(Create));
+            }
+
             if (ModelState.IsValid)
             {
+                var game = new Game
+                {
+                    PublisherId = model.PublisherId,
+                    Title = model.Title,
+                    Description = model.Description,
+                    Price = model.Price,
+                    QuantityInStock = model.QuantityInStock,
+                    MaxQuantity = model.MaxQuantity,
+                    Accessibility = model.Accessibility,
+                    Discount = model.Discount,
+                    Pegi = model.Pegi,
+                    MainPhotoName = model.MainPhotoName,
+                    Photo1Name = model.Photo1Name,
+                    Photo2Name = model.Photo2Name,
+                    Photo3Name = model.Photo3Name,
+                    Photo4Name = model.Photo4Name,
+                };
+
                 _context.Add(game);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index");
             }
-            ViewData["PublisherId"] = new SelectList(_context.Publishers, "PublisherId", "Name", game.PublisherId);
-            return View(game);
+            //
+            foreach (var key in ModelState.Keys)
+            {
+                var modelStateEntry = ModelState[key];
+                foreach (var error in modelStateEntry.Errors)
+                {
+                    Console.WriteLine($"Field: {key}, Error: {error.ErrorMessage}");
+                }
+            }
+            //
+            return RedirectToAction(nameof(Create));
         }
+
+
 
         // GET: Games/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -155,14 +245,14 @@ namespace SystemWypozyczalniGier.Controllers
             {
                 _context.Games.Remove(game);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool GameExists(int id)
         {
-          return (_context.Games?.Any(e => e.GameId == id)).GetValueOrDefault();
+            return (_context.Games?.Any(e => e.GameId == id)).GetValueOrDefault();
         }
     }
 }
